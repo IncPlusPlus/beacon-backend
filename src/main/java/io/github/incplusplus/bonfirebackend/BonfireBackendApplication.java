9d9b10b35c9c4b973ef12cc4c54301feee3942cb
Ryan Cloherty <cloherty.ryan@gmail.com>
2022-01-30 13:30:48 -0500

    Initial commit

---
+++ src/main/java/io/github/incplusplus/bonfirebackend/BonfireBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.incplusplus.bonfirebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BonfireBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BonfireBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/incplusplus/bonfirebackend/BonfireBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.incplusplus.bonfirebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BonfireBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
