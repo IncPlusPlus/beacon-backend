9c2cfbf734e50910ffdb8d44086b491ed2b13785
Ryan Cloherty <cloherty.ryan@gmail.com>
2022-02-04 13:59:21 -0500

    Add OpenAPI support + Swagger UI for both services

---
+++ city/src/main/java/io/github/incplusplus/beacon/city/config/SpringdocConfig.java
@@ -0,0 +1,19 @@
+package io.github.incplusplus.beacon.city.config;
+
+import org.springdoc.core.SpringDocConfigProperties;
+import org.springdoc.core.SpringDocConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringdocConfig {
+  @Bean
+  SpringDocConfiguration springDocConfiguration() {
+    return new SpringDocConfiguration();
+  }
+
+  @Bean
+  public SpringDocConfigProperties springDocConfigProperties() {
+    return new SpringDocConfigProperties();
+  }
+}
