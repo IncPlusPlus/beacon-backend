ce195e734b387849663c11c417eb0a1b67f9418c
Ryan Cloherty <cloherty.ryan@gmail.com>
2022-02-26 17:14:00 -0500

    Change how StorageFileNotFoundException is handled

--- central-identity-server/src/main/java/io/github/incplusplus/beacon/centralidentityserver/exception/StorageFileNotFoundException.java
+++ central-identity-server/src/main/java/io/github/incplusplus/beacon/centralidentityserver/exception/StorageFileNotFoundException.java
@@ -1,12 +1,19 @@
 package io.github.incplusplus.beacon.centralidentityserver.exception;
 
-public class StorageFileNotFoundException extends StorageException {
+import java.net.URI;
+import org.zalando.problem.AbstractThrowableProblem;
+import org.zalando.problem.Status;
+import org.zalando.problem.ThrowableProblem;
+
+public class StorageFileNotFoundException extends AbstractThrowableProblem {
+  private static final URI TYPE =
+      URI.create("https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/404");
 
   public StorageFileNotFoundException(String message) {
-    super(message);
+    super(TYPE, "File not found", Status.NOT_FOUND, message);
   }
 
   public StorageFileNotFoundException(String message, Throwable cause) {
-    super(message, cause);
+    super(TYPE, "File not found", Status.NOT_FOUND, message, null, (ThrowableProblem) cause);
   }
 }
