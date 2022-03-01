8823cf6548147f4df2e53f060e7c94e21a134ec9
Ryan Cloherty <cloherty.ryan@gmail.com>
2022-03-01 10:51:38 -0500

    Changed the /city-cis-intercom/verify-user endpoint
    The endpoint will now take the username and password as query parameters instead of having an entire schema object for it to be sent as the request body. The endpoint will now also return a UserAccountDto object. This will be cached by the City for any requests that might require information about the user to complete. For instance, creating a new message will require the ID of the user to be known. However, the City is only aware of the username of the user is known during a request. The City will now obtain account info for any user who makes a request. This info will be retained and can be used by any services that need it.

--- central-identity-server/src/main/java/io/github/incplusplus/beacon/centralidentityserver/controller/CityInterserviceCommunicationsController.java
+++ central-identity-server/src/main/java/io/github/incplusplus/beacon/centralidentityserver/controller/CityInterserviceCommunicationsController.java
@@ -3,8 +3,10 @@ package io.github.incplusplus.beacon.centralidentityserver.controller;
 import io.github.incplusplus.beacon.centralidentityserver.generated.controller.CityInterserviceCommunicationsApi;
 import io.github.incplusplus.beacon.centralidentityserver.generated.dto.CreateTowerInviteRequestDto;
 import io.github.incplusplus.beacon.centralidentityserver.generated.dto.NewCityDto;
-import io.github.incplusplus.beacon.centralidentityserver.generated.dto.UsernameAndPasswordDto;
+import io.github.incplusplus.beacon.centralidentityserver.generated.dto.UserAccountDto;
+import io.github.incplusplus.beacon.centralidentityserver.mapper.UserMapper;
 import io.github.incplusplus.beacon.centralidentityserver.service.CityService;
+import io.github.incplusplus.beacon.centralidentityserver.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,15 +22,21 @@ public class CityInterserviceCommunicationsController implements CityInterservic
   private final UserDetailsService userDetailsService;
   private final PasswordEncoder passwordEncoder;
   private final CityService cityService;
+  private final UserService userService;
+  private final UserMapper userMapper;
 
   @Autowired
   public CityInterserviceCommunicationsController(
       UserDetailsService userDetailsService,
       PasswordEncoder passwordEncoder,
-      CityService cityService) {
+      CityService cityService,
+      UserService userService,
+      UserMapper userMapper) {
     this.userDetailsService = userDetailsService;
     this.passwordEncoder = passwordEncoder;
     this.cityService = cityService;
+    this.userService = userService;
+    this.userMapper = userMapper;
   }
 
   @Override
@@ -45,16 +53,20 @@ public class CityInterserviceCommunicationsController implements CityInterservic
   }
 
   @Override
-  public ResponseEntity<Void> verifyUser(UsernameAndPasswordDto credentials) {
+  public ResponseEntity<UserAccountDto> verifyUser(String username, String password) {
     UserDetails userDetails;
+    UserAccountDto userAccountDto;
     try {
       // TODO In the future, check these for null. Otherwise, a 500 happens.
-      userDetails = userDetailsService.loadUserByUsername(credentials.getUsername());
+      userDetails = userDetailsService.loadUserByUsername(username);
     } catch (UsernameNotFoundException e) {
       return ResponseEntity.notFound().build();
     }
-    if (passwordEncoder.matches(credentials.getPassword(), userDetails.getPassword())) {
-      return ResponseEntity.ok().build();
+    if (passwordEncoder.matches(password, userDetails.getPassword())) {
+      // We already performed a loadByUsername, so it should be pretty safe to get w/o isPresent()
+      //noinspection OptionalGetWithoutIsPresent
+      userAccountDto = userMapper.userToUserDto(userService.getAccountByUsername(username).get());
+      return ResponseEntity.ok(userAccountDto);
     } else {
       return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
--- city/src/main/java/io/github/incplusplus/beacon/city/security/LoginAuthenticationProvider.java
+++ city/src/main/java/io/github/incplusplus/beacon/city/security/LoginAuthenticationProvider.java
@@ -1,10 +1,12 @@
 package io.github.incplusplus.beacon.city.security;
 
-import io.github.incplusplus.beacon.city.generated.dto.UsernameAndPasswordDto;
+import io.github.incplusplus.beacon.city.generated.dto.UserAccountDto;
 import io.github.incplusplus.beacon.city.spring.AutoRegisterCity;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -24,6 +26,7 @@ import org.springframework.web.reactive.function.client.WebClientResponseExcepti
 @Slf4j
 public class LoginAuthenticationProvider implements AuthenticationProvider {
 
+  private static final Map<String, UserAccountDto> userInfoMap = new ConcurrentHashMap<>();
   private final AutoRegisterCity autoRegisterCity;
 
   public LoginAuthenticationProvider(AutoRegisterCity autoRegisterCity) {
@@ -53,30 +56,38 @@ public class LoginAuthenticationProvider implements AuthenticationProvider {
             .build();
 
     try {
-      client
-          .post()
-          .uri("/city-cis-intercom/verify-user")
-          .bodyValue(new UsernameAndPasswordDto().username(name).password(password))
-          .retrieve()
-          .onStatus(
-              httpStatus -> httpStatus.equals(HttpStatus.UNAUTHORIZED),
-              clientResponse -> {
-                throw new BadCredentialsException(
-                    "Invalid user credentials for account '" + name + "'.");
-              })
-          .onStatus(
-              httpStatus -> httpStatus.equals(HttpStatus.NOT_FOUND),
-              clientResponse -> {
-                throw new UsernameNotFoundException("Username '" + name + "' not found.");
-              })
-          .toBodilessEntity()
-          .block(Duration.ofSeconds(30));
+      UserAccountDto account =
+          client
+              .get()
+              .uri(
+                  uriBuilder ->
+                      uriBuilder
+                          .path("/city-cis-intercom/verify-user")
+                          .queryParam("username", name)
+                          .queryParam("password", password)
+                          .build())
+              .retrieve()
+              .onStatus(
+                  httpStatus -> httpStatus.equals(HttpStatus.UNAUTHORIZED),
+                  clientResponse -> {
+                    throw new BadCredentialsException(
+                        "Invalid user credentials for account '" + name + "'.");
+                  })
+              .onStatus(
+                  httpStatus -> httpStatus.equals(HttpStatus.NOT_FOUND),
+                  clientResponse -> {
+                    throw new UsernameNotFoundException("Username '" + name + "' not found.");
+                  })
+              .bodyToMono(UserAccountDto.class)
+              .block(Duration.ofSeconds(30));
+      // Cache the user's info for any endpoints that might need to know the user's ID from their
+      // name
+      userInfoMap.put(name, account);
       // Since we've gotten this far, the verify-user endpoint must have returned 200. Go ahead and
       // authorize the user.
       List<GrantedAuthority> grantedAuths = new ArrayList<>();
       grantedAuths.add(new SimpleGrantedAuthority("ROLE_USER"));
       return new UsernamePasswordAuthenticationToken(name, password, grantedAuths);
-
     } catch (WebClientResponseException | WebClientRequestException e) {
       log.error(
           "The City encountered an exception when trying to verify a user with the CIS at '"
@@ -95,5 +106,9 @@ public class LoginAuthenticationProvider implements AuthenticationProvider {
   @Override
   public boolean supports(Class<?> authentication) {
     return authentication.equals(UsernamePasswordAuthenticationToken.class);
+  }
+
+  public String getIdForUsername(String username) {
+    return userInfoMap.get(username).getId();
   }
 }
