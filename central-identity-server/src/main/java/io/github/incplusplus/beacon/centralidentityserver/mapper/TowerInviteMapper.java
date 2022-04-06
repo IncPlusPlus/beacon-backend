ad16e65e52bbd2d02676228db78b23c3efa9d3fb
Ryan Cloherty <cloherty.ryan@gmail.com>
2022-04-06 16:11:50 -0400

    Don't bother giving the internal identity ID to any DTO instances

--- central-identity-server/src/main/java/io/github/incplusplus/beacon/centralidentityserver/mapper/TowerInviteMapper.java
+++ central-identity-server/src/main/java/io/github/incplusplus/beacon/centralidentityserver/mapper/TowerInviteMapper.java
@@ -3,10 +3,12 @@ package io.github.incplusplus.beacon.centralidentityserver.mapper;
 import io.github.incplusplus.beacon.centralidentityserver.generated.dto.TowerInviteDto;
 import io.github.incplusplus.beacon.centralidentityserver.persistence.model.TowerInvite;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface TowerInviteMapper {
   TowerInviteDto towerInviteToTowerInviteDto(TowerInvite invite);
 
+  @Mapping(target = "id", ignore = true)
   TowerInvite towerInviteDtoToTowerInvite(TowerInviteDto dto);
 }
