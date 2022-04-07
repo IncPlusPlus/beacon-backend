de5c5b86a0e7944a1f694e9a81d6c56fc6eef678
Ryan Cloherty <cloherty.ryan@gmail.com>
2022-04-07 13:37:05 -0400

    Implement user accessible endpoints on the City

--- city/src/main/java/io/github/incplusplus/beacon/city/controller/InviteController.java
+++ city/src/main/java/io/github/incplusplus/beacon/city/controller/InviteController.java
@@ -2,36 +2,52 @@ package io.github.incplusplus.beacon.city.controller;
 
 import io.github.incplusplus.beacon.city.generated.controller.InvitesApi;
 import io.github.incplusplus.beacon.city.generated.dto.TowerInviteDto;
+import io.github.incplusplus.beacon.city.security.IAuthenticationFacade;
+import io.github.incplusplus.beacon.city.service.InviteService;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class InviteController implements InvitesApi {
+  private final InviteService inviteService;
+  private final IAuthenticationFacade authenticationFacade;
+
+  @Autowired
+  public InviteController(InviteService inviteService, IAuthenticationFacade authenticationFacade) {
+    this.inviteService = inviteService;
+    this.authenticationFacade = authenticationFacade;
+  }
 
   @Override
   public ResponseEntity<TowerInviteDto> createInvite(
       String towerId, Integer expiryTime, String expiryTimeUnit, Integer maxUses) {
-    // TODO: Implement creating invites (will require CIS communication in the service)
-    return null;
+    return new ResponseEntity<>(
+        inviteService.createInvite(
+            authenticationFacade.getAuthentication().getName(),
+            towerId,
+            expiryTime,
+            expiryTimeUnit,
+            maxUses),
+        HttpStatus.CREATED);
   }
 
   @Override
   public ResponseEntity<Void> joinUsingInvite(String towerInviteCode) {
-    // TODO: Implement joining the relevant Tower by using the invite code.
-    //  Utilize towerInviteCode (verify it and use it with the CIS in the service layer).
-    return null;
+    inviteService.joinTowerUsingInvite(
+        authenticationFacade.getAuthentication().getName(), towerInviteCode);
+    return ResponseEntity.noContent().build();
   }
 
   @Override
   public ResponseEntity<List<TowerInviteDto>> listInvites(String towerId) {
-    // TODO: Implement listing invites (will require CIS communication in the service)
-    return null;
+    return ResponseEntity.ok(inviteService.listInvitesForTower(towerId));
   }
 
   @Override
   public ResponseEntity<TowerInviteDto> revokeInvite(String towerId, String towerInviteCode) {
-    // TODO: Implement revoking invites (will require CIS communication in the service)
-    return null;
+    return ResponseEntity.ok(inviteService.revokeInvite(towerId, towerInviteCode));
   }
 }
