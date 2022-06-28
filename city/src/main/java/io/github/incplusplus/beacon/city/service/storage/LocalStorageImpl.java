b7605e7a59601cd1050ca87cea5d64c8ae3f8b95
Ryan Cloherty <cloherty.ryan@gmail.com>
2022-06-28 14:27:03 -0400

    Fix the same issue for the Tower banner and icon
    In theory, this same issue could happen with files that will always be named icon.png and banner.png so this AWFUL fix should prevent that from happening. I think this is a complete fix, but I can't look at this anymore so I'm just gonna push it.

--- city/src/main/java/io/github/incplusplus/beacon/city/service/storage/LocalStorageImpl.java
+++ city/src/main/java/io/github/incplusplus/beacon/city/service/storage/LocalStorageImpl.java
@@ -5,21 +5,27 @@ import io.github.incplusplus.beacon.city.properties.LocalStorageProperties;
 import io.github.incplusplus.beacon.city.service.StorageService;
 import io.github.incplusplus.beacon.city.spring.AutoRegisterCity;
 import io.github.incplusplus.beacon.common.exception.StorageException;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 /** The default storage provider for user-uploaded content. */
+@Slf4j
 @Service
 @ConditionalOnMissingBean(value = StorageService.class, ignored = LocalStorageImpl.class)
 public class LocalStorageImpl implements StorageService {
+  private final Pattern bannerOrIconUrlPattern;
 
   private final Path rootLocation;
 
@@ -29,6 +35,11 @@ public class LocalStorageImpl implements StorageService {
   public LocalStorageImpl(LocalStorageProperties properties, AutoRegisterCity autoRegisterCity) {
     this.rootLocation = Paths.get(properties.getLocation());
     this.autoRegisterCity = autoRegisterCity;
+    bannerOrIconUrlPattern =
+        Pattern.compile(
+            "https://"
+                + autoRegisterCity.getHostName()
+                + "/attachments/([a-f\\d]+)/(icon|banner)-(\\d+)\\.png");
   }
 
   @Override
@@ -71,17 +82,24 @@ public class LocalStorageImpl implements StorageService {
   }
 
   @Override
-  public String saveTowerIcon(MultipartFile icon, String towerId) {
-    return saveTowerIconOrBanner(icon, towerId, true);
+  public String saveTowerIcon(MultipartFile icon, String towerId, String existingUrl) {
+    return saveTowerIconOrBanner(icon, towerId, true, existingUrl);
   }
 
   @Override
-  public String saveTowerBanner(MultipartFile banner, String towerId) {
-    return saveTowerIconOrBanner(banner, towerId, false);
+  public String saveTowerBanner(MultipartFile banner, String towerId, String existingUrl) {
+    return saveTowerIconOrBanner(banner, towerId, false, existingUrl);
   }
 
-  private String saveTowerIconOrBanner(MultipartFile file, String towerId, boolean isIcon) {
-    String fileName = isIcon ? "icon.png" : "banner.png";
+  private String saveTowerIconOrBanner(
+      MultipartFile file, String towerId, boolean isIcon, String existingUrl) {
+    // Clean up the older icon or banner file
+    deleteOldBannerOrIcon(existingUrl);
+
+    // This isn't exactly the smartest way to use a TSID, but it's good enough for now.
+    long tsid = TsidCreator.getTsid256().toLong();
+    String fileName = (isIcon ? "icon" : "banner") + "-" + tsid + ".png";
+
     Path attachmentPath = Paths.get(towerId, fileName);
     try {
       if (file.isEmpty()) {
@@ -113,5 +131,22 @@ public class LocalStorageImpl implements StorageService {
         // I know this sucks. Shut up.
         + "/attachments/"
         + attachmentPath.toString().replace("\\", "/");
+  }
+
+  private void deleteOldBannerOrIcon(String url) {
+    Matcher matcher = bannerOrIconUrlPattern.matcher(url);
+    if (!matcher.find()) {
+      throw new IllegalStateException("Couldn't determine previous URL for banner or icon.");
+    }
+    Path target =
+        this.rootLocation
+            .resolve(
+                Paths.get(matcher.group(1), matcher.group(2) + "-" + matcher.group(3) + ".png"))
+            .normalize()
+            .toAbsolutePath();
+    File file = target.toFile();
+    if (!file.delete()) {
+      log.warn("Unable to delete file at " + file.getAbsolutePath());
+    }
   }
 }
